550577caa40dc9097a489b6e4c07f0eca96b972c
Sanketh S Huddar <40519714+Sankjain@users.noreply.github.com>
2019-07-19 01:39:12 +0530

    Initial commit

---
+++ index.php
@@ -0,0 +1,516 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- ========== Meta Tags ========== -->
+    <meta charset="UTF-8">
+    <meta name="description" content="Evento -Event Html Template">
+    <meta name="keywords" content="Evento , Event , Html, Template">
+    <meta name="author" content="ColorLib">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <!-- ========== Title ========== -->
+    <title> BCIC - AccelATHON</title>
+    <!-- ========== Favicon Ico ========== -->
+    <link rel="icon" href="bcic.ico">
+    <!-- ========== STYLESHEETS ========== -->
+    <!-- Bootstrap CSS -->
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Fonts Icon CSS -->
+    <link href="assets/css/font-awesome.min.css" rel="stylesheet">
+    <link href="assets/css/et-line.css" rel="stylesheet">
+    <link href="assets/css/ionicons.min.css" rel="stylesheet">
+    <!-- Carousel CSS -->
+    <link href="assets/css/owl.carousel.min.css" rel="stylesheet">
+    <link href="assets/css/owl.theme.default.min.css" rel="stylesheet">
+    <!-- Animate CSS -->
+    <link rel="stylesheet" href="assets/css/animate.min.css">
+    <!-- Custom styles for this template -->
+    <link href="assets/css/main.css" rel="stylesheet">
+</head>
+<body>
+<div class="loader">
+    <div class="loader-outter"></div>
+    <div class="loader-inner"></div>
+</div>
+
+<!--header start here -->
+<header class="header navbar fixed-top navbar-expand-md">
+    <div class="container">
+        <a class="navbar-brand logo" href="#">
+            <img src="assets/img/bcic.jpg" alt="Evento"/>
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#headernav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="lnr lnr-text-align-right"></span>
+        </button>
+        <div class="collapse navbar-collapse flex-sm-row-reverse" id="headernav">
+            <ul class=" nav navbar-nav menu">
+                <li class="nav-item">
+                    <a class="nav-link active" href="#home">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link " href="#about">About</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link " href="#sche">Schedule</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link " href="#reg">Register</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link " href="#cont">Contact</a>
+                </li>
+                
+            </ul>
+        </div>
+    </div>
+</header>
+<!--header end here-->
+
+<!--cover section slider -->
+<section id="home" class="home-cover">
+    <div class="cover_slider owl-carousel owl-theme">
+        <div class="cover_item" style="background: url('assets/img/bg/slider.png');">
+             <div class="slider_content">
+                <div class="slider-content-inner">
+                    <div class="container">
+                        <div class="slider-content-center">
+                            <h2 class="cover-title">
+                                BCIC Brings the Digi Hackathon
+                            </h2>
+                            <strong class="cover-xl-text">accelATHON</strong>
+                            <p class="cover-date">
+                                12-14 April 2019  - New Horizon College Of Engineering.
+                            </p>
+                            <a href="https://docs.google.com/forms/d/e/1FAIpQLSes3J0n-28B4i6IiudwPylVEpsru0_dZfxSmc7Euu_QTnidxQ/viewform?vc=0&c=0&w=1" class=" btn btn-primary btn-rounded" >
+                                Register Now
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+       
+
+    </div>
+ 
+</section>
+<!--cover section slider end -->
+
+<!--event info -->
+<section class="pt100 pb100">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-6 col-md-3  ">
+                <div class="icon_box_two">
+                    <i class="ion-ios-calendar-outline"></i>
+                    <div class="content">
+                        <h5 class="box_title">
+                            DATE
+                        </h5>
+                        <p>
+                            12-14 March 2019
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-6 col-md-3  ">
+                <div class="icon_box_two">
+                    <i class="ion-ios-location-outline"></i>
+                    <div class="content">
+                        <h5 class="box_title">
+                            location
+                        </h5>
+                        <p>
+                            New Horizon College Of Engineering
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+        
+            <div class="col-6 col-md-3  ">
+                <div class="icon_box_two">
+                    <i class="ion-ios-pricetag-outline"></i>
+                    <div class="content">
+                        <h5 class="box_title">
+                            Registration
+                        </h5>
+                        <p>
+                            Rs. 300 
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!--event info end -->
+
+
+<!--event countdown -->
+<section id="about" class="bg-img pt70 pb70" style="background-image: url('assets/img/bg/bg-img.png');">
+    <div class="overlay_dark"></div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-12 col-md-10">
+                <h4 class="mb30 text-center color-light">Counter until the big event</h4>
+                <div class="countdown"></div>
+            </div>
+        </div>
+    </div>
+</section>
+<!--event count down end-->
+
+
+<!--about the event -->
+<section class="pt100 pb100">
+    <div class="container">
+        <div class="section_title">
+            <h3 class="title">
+                About the event
+            </h3>
+        </div>
+        <div class="row justify-content-center">
+            <div class="col-12 col-md-6">
+                <p>
+                   Bangalore Chamber of Industry & Commerce (BCIC) and New Horizon College of Engineering (NHCE) are organising a Karnataka-wide Inter-College Hackathon (“AccelATON”) for Engineering Students spanning 3 days on the 12th to 14th April, 2019.
+                </p>
+            </div>
+            <div class="col-12 col-md-6">
+                <p>
+                   The theme of this Hackathon is to find Solutions to various Social Issues through Innovative mechanisms using Digital Technologies.The Hackathon will be made available to Students of any branch of engineering and from any year/semester. This Hackathon is open to all Engineering Colleges in Karnataka. Colleges are invited to participate by sending their best teams.
+                </p>
+            </div>
+        </div>
+		
+		 <div class="section_title">
+            <h3 class="title">
+               <br> Take Aways
+            </h3>
+        </div>
+
+        <!--event features-->
+        <div class="row justify-content-center mt30">
+            <div class="col-12 col-md-6 col-lg-3">
+                <div class="icon_box_one">
+                    <i class="lnr lnr-mic"></i>
+                    <div class="content">
+                        <h4>Cash Prizes</h4>
+                        <p>
+                            Rewards to 6 Winners <br>
+							1 x First Place <br>	
+							2 x Second Place <br>
+							3 x Third Place
+                        </p>
+                       
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-12 col-md-6 col-lg-3">
+                <div class="icon_box_one">
+                    <i class="lnr lnr-rocket"></i>
+                    <div class="content">
+                        <h4>48hrs Coding</h4>
+                        <p>
+                            Code straight for 48 hours and hack the problem statements to gain the benefits.
+                        </p>
+                       
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-12 col-md-6 col-lg-3">
+                <div class="icon_box_one">
+                    <i class="lnr lnr-bullhorn"></i>
+                    <div class="content">
+                        <h4>Internships</h4>
+                        <p>
+                            Get a chance to get Internship opportunities and work with the top companies.
+                        </p>
+                     
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-12 col-md-6 col-lg-3">
+                <div class="icon_box_one">
+                    <i class="lnr lnr-clock"></i>
+                    <div class="content">
+                        <h4>Get Hired</h4>
+                        <p>
+                            Give your best and get a chance to get hired by the Top Companies present at the event.
+              
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!--event features end-->
+    </div>
+</section>
+<!--about the event end -->
+
+
+
+
+
+
+<!--event calender-->
+<section id ="sche" class="pb100">
+    <div class="container">
+        <div class="table-responsive">
+            <table class="event_calender table">
+                <thead class="event_title">
+                <tr>
+                    <th>
+                        <i class="ion-ios-calendar-outline"></i>
+                        <span>next events calendar</span>
+                    </th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td>
+                        <img src="assets/img/cleander/c1.png" alt="event">
+                    </td>
+                    <td class="event_date">
+                       12
+                        <span>March</span>
+                    </td>
+                    <td>
+                        <div class="event_place">
+                            <h5>Crowd Address Speech</h5>
+                            <h6>09 AM - 1 AM</h6>
+                          
+                        </div>
+                    </td>
+                    <td>
+                        <a href="#" class="btn btn-primary btn-rounded">Download Brochure</a>
+                    </td>
+                    <td class="buy_link">
+                        
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <img src="assets/img/cleander/c2.png" alt="event">
+                    </td>
+                    <td class="event_date">
+                        12-14
+                        <span>March</span>
+                    </td>
+                    <td>
+                        <div class="event_place">
+                            <h5>Digital Hackathon</h5>
+                            <h6>2 PM(12th) - 1 PM(14th)</h6>
+                            
+                        </div>
+                    </td>
+                    <td>
+                        <a href="#" class="btn btn-primary btn-rounded">Download Rules and Info</a>
+                    </td>
+                    <td class="buy_link">
+                       
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <img src="assets/img/cleander/c3.png" alt="event">
+                    </td>
+                    <td class="event_date">
+                        14
+                        <span>March</span>
+                    </td>
+                    <td>
+                        <div class="event_place">
+                            <h5>Interview and Results</h5>
+                            <h6>1:30 PM - 4:30 PM</h6>
+                         
+                        </div>
+                    </td>
+                    <td>
+                        <a href="#" class="btn btn-primary btn-rounded">Download Details</a>
+                    </td>
+                    <td class="buy_link">
+                      
+                    </td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</section>
+<!--event calender end -->
+
+<!--
+<section class="bg-gray pt100 pb100">
+    <div class="container">
+        <div class="section_title mb50">
+            <h3 class="title">
+                our partners
+            </h3>
+        </div>
+        <div class="brand_carousel owl-carousel">
+            <div class="brand_item text-center">
+                <img src="assets/img/brands/b1.png" alt="brand">
+            </div>
+            <div class="brand_item text-center">
+                <img src="assets/img/brands/b2.png" alt="brand">
+            </div>
+
+            <div class="brand_item text-center">
+                <img src="assets/img/brands/b3.png" alt="brand">
+            </div>
+            <div class="brand_item text-center">
+                <img src="assets/img/brands/b4.png" alt="brand">
+            </div>
+            <div class="brand_item text-center">
+                <img src="assets/img/brands/b5.png" alt="brand">
+            </div>
+        </div>
+    </div>
+</section>
+-->
+
+<!--get tickets section -->
+<section id="reg" class="bg-img pt100 pb100" style="background-image: url('assets/img/bg/tickets.png');">
+    <div class="container">
+        <div class="section_title mb30">
+            <h3 class="title color-light">
+               Register Now to enter the DIGITAL WORLD
+            </h3>
+        </div>
+        <div class="row justify-content-center align-items-center">
+            <div class="col-md-9 text-md-left text-center color-light">
+              Bangalore Chamber of Industry & Commerce (BCIC) and New Horizon College of Engineering (NHCE) are organising a Karnataka-wide Inter-College Hackathon (“AccelATON”) for Engineering Students spanning 3 days on the 12th to 14th April, 2019.
+            </div>
+            <div class="col-md-3 text-md-right text-center">
+                <a href="https://docs.google.com/forms/d/e/1FAIpQLSes3J0n-28B4i6IiudwPylVEpsru0_dZfxSmc7Euu_QTnidxQ/viewform?vc=0&c=0&w=1" class="btn btn-primary btn-rounded mt30">Register Now</a>
+            </div>
+        </div>
+    </div>
+</section>
+<!--get tickets section end-->
+
+<!--footer start -->
+<section id="cont">
+<footer>
+    <div class="container">
+        <div class="row justify-content-center">
+
+            <div class="col-md-4 col-12">
+                <div class="footer_box">
+                    <div class="footer_header">
+                       
+                    </div>
+                    <div class="footer_box_body">
+                        <p>
+                            BCIC (earlier known as Greater Mysore Chamber of Industry (GMCI)) is the Apex Chamber of Commerce representing large and medium industries in the State of Karnataka.
+
+The Chamber represents 95% of the capital investment and 90% of labour employed in the State of Karnataka. We have presently a membership of 750 companies representing various sectors of industry
+                        </p>
+                        <ul class="footer_social">
+                            <li>
+                                <a href="#"><i class="ion-social-pinterest"></i></a>
+                            </li>
+                            <li>
+                                <a href="#"><i class="ion-social-facebook"></i></a>
+                            </li>
+                            <li>
+                                <a href="#"><i class="ion-social-twitter"></i></a>
+                            </li>
+                            <li>
+                                <a href="#"><i class="ion-social-dribbble"></i></a>
+                            </li>
+                            <li>
+                                <a href="#"><i class="ion-social-instagram"></i></a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-12 col-md-4">
+                <div class="footer_box">
+                    <div class="footer_header">
+                    <img src="assets/img/bcic.jpg" alt="evento">
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-12 col-md-4">
+                <div class="footer_box">
+                    <div class="footer_header">
+                        <h4 class="footer_title">
+                            subscribe to our newsletter
+                        </h4>
+                    </div>
+                    <div class="footer_box_body">
+                        <div class="newsletter_form">
+                            <input type="email" class="form-control" placeholder="E-Mail here">
+                            <button class="btn btn-rounded btn-block btn-primary">SUBSCRIBE</button>
+							<br>Email ID : bcic.karnataka@gmail.com <br><br>
+							Phone No : Sanketh - 9740496061
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+</section>
+<div class="copyright_footer">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-6 col-12">
+                <p>
+Copyright &copy; BCIC 2019 All rights reserved </i> 
+</p>
+            </div>
+            <div class="col-12 col-md-6 ">
+                <ul class="footer_menu">
+                    <li>
+                        <a href="#home">Home</a>
+                    </li>
+                    <li>
+                        <a href="#about">About</a>
+                    </li>
+                    <li>
+                        <a href="#sche">Schedule</a>
+                    </li>
+                    <li>
+                        <a href="#reg">Register</a>
+                    </li>
+                    <li>
+                        <a href="#cont">Contact</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+<!--footer end -->
+
+<!-- jquery -->
+<script src="assets/js/jquery.min.js"></script>
+<!-- bootstrap -->
+<script src="assets/js/popper.js"></script>
+<script src="assets/js/bootstrap.min.js"></script>
+<script src="assets/js/waypoints.min.js"></script>
+<!--slick carousel -->
+<script src="assets/js/owl.carousel.min.js"></script>
+<!--parallax -->
+<script src="assets/js/parallax.min.js"></script>
+<!--Counter up -->
+<script src="assets/js/jquery.counterup.min.js"></script>
+<!--Counter down -->
+<script src="assets/js/jquery.countdown.min.js"></script>
+<!-- WOW JS -->
+<script src="assets/js/wow.min.js"></script>
+<!-- Custom js -->
+<script src="assets/js/main.js"></script>
+</body>
+</html>
